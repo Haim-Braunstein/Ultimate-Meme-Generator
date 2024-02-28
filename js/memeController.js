c6a70c6c7bf223b58503c6a0d0b1b516fc72b3ba
Haim Braunstein <haim321@gmail.com>
2024-02-28 14:45:20 +0200

    Add seLineText to input user text

--- js/memeController.js
+++ js/memeController.js
@@ -5,35 +5,37 @@ let gCtx
 
 
 function onInit() {
-    renderMeme()
-}
-
-function renderMeme() {
-    const mems =  getMeme()
- 
     gElCanvas = document.querySelector('.img-canvas')
     gCtx = gElCanvas.getContext('2d')
 
-    gCtx.clearRect(0, 0, gElCanvas.width, gElCanvas.height)
-    
-    gCtx.font = "30px Arial"
-	gCtx.fillStyle = 'black'
-    gCtx.fillText(mems.lines[0].txt, 30, 80)
-
-    drawImg()
-   
-
+    renderMeme()
 }
 
 
-function drawImg() {
+function renderMeme() {
+    const mems =  getMeme()
     const elImg = new Image()
     elImg.src = 'img/2.jpg'
 
-    // Draw the image on the canvas only when it's ready
-
-    elImg.onload = () =>
+    elImg.onload = () =>{
         gCtx.drawImage(elImg, 0, 0, gElCanvas.width, gElCanvas.height)
+        gCtx.font = "30px impact"
+	gCtx.fillStyle = 'black'
+    console.log(mems.lines[0].txt);
+    gCtx.fillText(mems.lines[0].txt, 10, 50)
+    }
+    
 }
+
+function onTextMeme(txt){
+    setLineText(txt)
+    renderMeme()
+}
+
+
+
+
+
+
 
 
--- js/service/meme.service.js
+++ js/service/meme.service.js
@@ -4,16 +4,24 @@ var gImgs = [
     { id: 1, url: 'img/1.jpg', keywords: ['funny', 'cat'] }
 ]
 
-var gMeme
+var gMeme = {
+    selectedImgId: 5,
+    selectedLineIdx: 0,
+    lines: [
+        { txt: 'I sometimes eat Falafel', size: 20, color: 'red' }
+    ]
+}
 
 var gKeywordSearchCountMap = { 'funny': 12, 'cat': 16, 'baby': 2 }
 
 function getMeme(){
-    return {
-        selectedImgId: 5,
-        selectedLineIdx: 0,
-        lines: [
-            { txt: 'I sometimes eat Falafel', size: 20, color: 'red' }
-        ]
-    }
+    
+    return  gMeme 
+    
+}
+
+function setLineText (text){
+
+    gMeme.lines[0].txt= text
+
 }
